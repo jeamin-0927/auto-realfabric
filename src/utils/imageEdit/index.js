68623fbdb63a1111b20ac6e9c5d6eaaa8de1c764
최재민 <admin@chicken-moo.com>
2023-02-21 15:43:44 +0900

    :zap: slpit and slpot add

--- src/utils/imageEdit/index.js
+++ src/utils/imageEdit/index.js
@@ -1,15 +1,28 @@
 const mergeImages = require('merge-images');
 const { Canvas, Image } = require('canvas');
+const sharp = require('sharp');
 const fs = require("fs");
 // install canvas on node -> https://github.com/Automattic/node-canvas
 
+const mergeSrc = (src) => `data:image/png;base64,${src}`;
+
 const imageEdit = async (src = './view/test') => {
   const rowImage = `${src}/image.jpg`;
+
+  const bRow = await sharp(rowImage).flop(true).toBuffer();
+  const b = Buffer.from(bRow).toString('base64');
+
+  const cRow = await sharp(rowImage).flip(true).toBuffer();
+  const c = Buffer.from(cRow).toString('base64');
+  
+  const dRow = await sharp(rowImage).flop(true).flip(true).toBuffer();
+  const d = Buffer.from(dRow).toString('base64');
+
   const mergeImage = await mergeImages([
     { src: rowImage, x: 0, y: 0 },
-    { src: rowImage, x: 1024, y: 0 },
-    { src: rowImage, x: 0, y: 1024 },
-    { src: rowImage, x: 1024, y: 1024 },
+    { src: mergeSrc(b), x: 1024, y: 0 },
+    { src: mergeSrc(c), x: 0, y: 1024 },
+    { src: mergeSrc(d), x: 1024, y: 1024 },
   ], {
     Canvas: Canvas,
     Image: Image,
@@ -20,7 +33,7 @@ const imageEdit = async (src = './view/test') => {
   const buffer = Buffer.from(mergeImage.split(`data:image/png;base64,`)[1], "base64");
   fs.writeFileSync(`${src}/output.jpg`, buffer);
 
-  // return mergeImage.split(`data:image/png;base64,`)[1];
+  return mergeImage;
 };
 
 module.exports = imageEdit;
